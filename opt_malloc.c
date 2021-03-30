0c5098e0d1ad2ec60cae0338d560d8e5b7c67c37
JP Memon <johnmemon@gmail.com>
2021-03-30 16:43:50 -0400

    Bitmap Buckets - Compiles

--- opt_malloc.c
+++ opt_malloc.c
@@ -8,20 +8,28 @@
 
 // Used whether free or in use
 // if in use, next and prev of surrounding entries will skip one in use
-typedef struct chunk {
+typedef struct bkt_t {
 	size_t size;
-	//struct chunk* prev;
-	struct chunk* next;
-} chunk;
+	void* mem;
+	long used_map[8];
+	struct bkt_t* next; 
+} bkt_t;
+
+typedef struct large_chunk_t {
+	size_t size;
+} large_chunk_t;
 
 long NUM_BUCKETS = 19;
-chunk* buckets[19];    // initially all NULL since global
 size_t bucket_sizes[] = {8, 12, 16, 24, 32, 48, 64, 96, 128, 192,
 						 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096};
 
+bkt_t* bkt_list = NULL;
+void* BKT_MEM = NULL;
+size_t BKT_MEM_OFF = 0;
+size_t BKT_MEM_LEN = 0;
+
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 const size_t PAGE_SIZE = 4096;
-const size_t MIN_ALLOCATION = 8;
 
 long
 bucket(size_t size)
@@ -34,7 +42,7 @@ bucket(size_t size)
 			minSize = bucket_sizes[++b_idx];
 	}
 
-	return b_idx;
+	return bucket_sizes[b_idx];
 }
 
 static
@@ -50,178 +58,222 @@ div_up(size_t xx, size_t yy)
 
 }
 
-long
-bucket_add(void* addr, long b_idx)
+/*
+Returns bkt_t* if mem is within that bucket
+Returns NULL if mem was a large allocation
+@params: void* mem
+*/
+bkt_t*
+bucket_memfind(void* ptr)
 {
-	chunk* toAdd = (chunk*)(addr);
-	chunk* tmp = buckets[b_idx];
-	chunk* tmp_prev = NULL;
-	long insIdx = 0;
+	bkt_t* tmp = bkt_list;
 
-	if (tmp && (uintptr_t)tmp < (uintptr_t)addr) {
+	while (tmp) {
+		if ((uintptr_t)tmp->mem <= (uintptr_t)ptr && 
+			(uintptr_t)ptr <= (uintptr_t)tmp->mem + PAGE_SIZE)
+			break;
 
-		while (tmp && (uintptr_t)tmp < (uintptr_t)addr) {
-			tmp_prev = tmp;
-			tmp = tmp->next;
-			insIdx += 1;
+		tmp = tmp->next;
+	}
+
+	return tmp;
+}
+
+/*
+Returns bucket of size if it is in bkt_list and has free entries
+Returns NULL otherwise
+Assumes size is one of potential bucket sizes
+@params: size_t size
+*/
+bkt_t*
+bucket_find(size_t size)
+{
+	bkt_t* tmp = bkt_list;
+	long bm_max = PAGE_SIZE / size;
+	long chk = 0;
+
+	while (tmp) {
+
+		if (tmp->size == size) {
+			
+			for (int i = 0; i < bm_max; i++) {
+				
+				if (((tmp->used_map[chk >> 6] >> (chk & 0x40)) & 0x1) == 0)
+					break;
+
+				if (++chk == bm_max)
+					goto next;
+			}
+
+			return tmp;
 		}
 
-		toAdd->size = bucket_sizes[b_idx];
-		toAdd->next = tmp;
-		if (tmp_prev)
-			tmp_prev->next = toAdd;
-	}
-	else {
-		toAdd->size = bucket_sizes[b_idx];
-		toAdd->next = buckets[b_idx];
-		buckets[b_idx] = toAdd;
-	}
-
-	return insIdx;
-}
-
-void
-bucket_delete(long b_idx, long idx)
-{
-	chunk* tmp = buckets[b_idx];
-
-	printf("idx: %ld\n", idx);
-	if (idx == 0) {
-		if (tmp)
-			buckets[b_idx] = tmp->next;
-
-		return;
-	}
-
-	while (--idx)
+		next:
 		tmp = tmp->next;
+	}
 
-	if (tmp && tmp->next)
-		tmp->next = tmp->next->next;
+	return NULL;
 }
 
-void
-bucket_coalesce()
+/*
+Returns address of free chunk from bkt
+Assumes bkt has free entries
+Modifies bitmap to reflect fact that new entry is being used
+@params: bkt_t* bkt
+*/
+void*
+bucket_get(bkt_t* bkt)
 {
-	chunk* tmp = NULL;
-	chunk* tmp_prev = NULL;
-	char del = 0;
+	bkt_t* tmp = bkt_list;
+
+	void* ptr = NULL;
+	long bm_max = PAGE_SIZE / bkt->size;
+	long off = 0;
+	
+	for (int i = 0; i < bm_max; i++) {
+				
+		if (((tmp->used_map[off >> 6] >> (off & 0x40)) & 0x1) == 0) {
+			ptr = bkt->mem + off * bkt->size;
+			tmp->used_map[off >> 6] |= 1 << (off & 0x40);
+			break;
+		}
+
+		off += 1;
+	}
+
+	return ptr;
+}
+
+/*
+Inserts bucket of size b_size into bkt_list
+Constructs bucket
+Assumes b_size is one of sizes in bucket_sizes
+Orders bucket list in size order from least to greatest
+New buckets go at the end of the run of buckets of their size
+@params: bkt_t* bkt
+*/
+void
+bucket_insert(long b_size)
+{
+	if (!BKT_MEM) {
+		BKT_MEM = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+		BKT_MEM_LEN = PAGE_SIZE;
+	}
+
+	if (BKT_MEM_LEN <= BKT_MEM_OFF + sizeof(bkt_t)) {
+
+		void* bmem_tmp = mmap(NULL, BKT_MEM_LEN + PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+		memcpy(bmem_tmp, BKT_MEM, BKT_MEM_OFF);
+
+		int err = munmap(BKT_MEM, BKT_MEM_LEN);
+		if (err == -1)
+			perror("bucket_insert: munmap() failed");
+
+		BKT_MEM = bmem_tmp;
+		BKT_MEM_LEN += PAGE_SIZE;
+	}
+
+	bkt_t* tmp = bkt_list;
+	bkt_t* tmp_prev = NULL;
+
+	bkt_t* bkt = (bkt_t*)(BKT_MEM + BKT_MEM_OFF);
+	bkt->size = b_size;
+
+	void* addr = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	bkt->mem = addr;
+
+	for (int i = 0; i < 8; i++)
+		bkt->used_map[i] = 0;
+
 	long idx = 0;
+	while (tmp && tmp->size <= bkt->size) {
+		tmp_prev = tmp;
+		tmp = tmp->next;
+		idx += 1;
+	}
 
-	for (int i = 0; i < NUM_BUCKETS; i++) {
+	if (idx == 0)
+		bkt_list = bkt;
 
-		do {
+	if (tmp_prev)
+		tmp_prev->next = bkt;
 
-			tmp = buckets[i];
-			tmp_prev = NULL;
-			idx = 0;
+	bkt->next = tmp;
 
-			del = 0;
+	BKT_MEM_OFF += sizeof(bkt_t);
+}
 
-			while (tmp) {
+/*
+Finds spot mem belongs at and sets it used bit to 0,
+putting it back in the bucket for later use
+Assumes mem is within bkt
+@params: void* mem, bkt_t* bkt
+*/
+void
+bucket_put(void* mem, bkt_t* bkt)
+{
+	uintptr_t mem_off = (uintptr_t)mem - (uintptr_t)bkt;
+	long idx = mem_off / bkt->size;
+	long inner_idx = idx >> 6;
 
-				if (i < NUM_BUCKETS - 1 && tmp_prev && (uintptr_t)(tmp_prev) + tmp_prev->size == (uintptr_t)(tmp)) {
-					bucket_delete(i, idx); // remove tmp_prev and tmp from lower list
-					bucket_delete(i, idx - 1);
-					bucket_add((void*)tmp_prev, i + 1); // add tmp_prev to next list up
-
-					idx -= 1;
-					del = 1;
-				}
-
-				idx += 1;
-				tmp_prev = tmp;
-				tmp = tmp->next;
-			} // end while
-
-		} while (del == 1);
-
-	} // end for
-
-} // end bucket_coalesce
+	bkt->used_map[inner_idx] &= ~(1 << inner_idx);
+}
 
 void*
 xmalloc(size_t bytes)
 {
-	bytes += sizeof(chunk);
-
 	void* ptr = NULL;
 
 	if (bytes <= PAGE_SIZE) {
 		pthread_mutex_lock(&mutex);
 
-		long b_idx = bucket(bytes);
-		chunk* tmp = buckets[b_idx];
+		long b_size = bucket(bytes);
+		bkt_t* bkt = bucket_find(b_size);
 
-		if (tmp) {
-			ptr = (void*)(tmp); // header filled out when added to list
-			bucket_delete(b_idx, 0);
-		}
-		// No entry of that size in list, so look for available larger chunks to split
+		if (bkt)
+			ptr = bucket_get(bkt);
 		else {
-
-			// get b_idx to first list with available chunks
-			long off = 0;
-			while (!buckets[b_idx + off]) {
-
-				if (b_idx + off == NUM_BUCKETS - 1) {
-					ptr = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-					bucket_add(ptr, b_idx + off);
-					break;
-				}
-
-				off += 1;
-			}
-
-			// Let memory flow down buckets
-			ptr = (void*)buckets[b_idx + off];
-			bucket_delete(b_idx + off, 0);
-
-			while (off) {
-				void* split = (void*)((uintptr_t)ptr + bucket_sizes[b_idx + --off]);
-				long bkt = bucket(bucket_sizes[b_idx + off + 1] - bucket_sizes[b_idx + off]);
-				bucket_add(split, bkt);
-			}
-
-			bucket_add(ptr, b_idx);
-
+			bucket_insert(b_size);
 			pthread_mutex_unlock(&mutex);
-			return xmalloc(bytes - sizeof(chunk));
+			return xmalloc(bytes);
 		}
 
 		pthread_mutex_unlock(&mutex);	
+		return ptr;
 	}
 	else {
+		bytes += sizeof(large_chunk_t);
 		size_t pages = div_up(bytes, PAGE_SIZE);
 
 		ptr = mmap(NULL, pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 		if (ptr == (void*)(-1))
 			perror("xmalloc: mmap() failed");
 
-		chunk* cPtr = (chunk*)(ptr);
-		cPtr->size = bytes;
-		cPtr->next = NULL;
+		large_chunk_t* cPtr = (large_chunk_t*)(ptr);
+		cPtr->size = pages * PAGE_SIZE;
+		
+		return (void*)cPtr + sizeof(large_chunk_t);
 	}
 
-	return ptr + sizeof(chunk);
+	return (void*)(-1);
 }
 
 void
 xfree(void* ptr)
 {
-	chunk* cPtr = (chunk*)(ptr - sizeof(chunk));
-	size_t size = cPtr->size;
-	
-	if (size <= PAGE_SIZE) {
-		long b_idx = bucket(cPtr->size);
+	bkt_t* bkt = bucket_memfind(ptr);
 
+	// ptr was small allocation
+	if (bkt) {
 		pthread_mutex_lock(&mutex);
-		bucket_add((void*)cPtr, b_idx);
-		bucket_coalesce();
+		bucket_put(ptr, bkt);
 		pthread_mutex_unlock(&mutex);
 	}
+	// ptr was large allocation
 	else {
-		int err = munmap(cPtr, size);
+		ptr -= sizeof(large_chunk_t);
+
+		int err = munmap(ptr, ((large_chunk_t*)(ptr))->size);
 		if (err == -1)
 			perror("xfree: munmap() failed");
 	}
@@ -231,39 +283,62 @@ xfree(void* ptr)
 void*
 xrealloc(void* prev, size_t bytes)
 {
-	bytes += sizeof(chunk);
-
-	chunk* cPtr = (chunk*)((uintptr_t)prev - sizeof(chunk));
 	void* ptr = NULL;
-	long b_idx_old = bucket(cPtr->size);
-	long b_idx_new = bucket(bytes);
+	bkt_t* bkt = bucket_memfind(prev);
 
-	if (bytes <= PAGE_SIZE) {
+	if (bkt && bytes <= PAGE_SIZE) {
 
-		if (b_idx_new == b_idx_old)
+		if (bytes == bkt->size)
 			ptr = prev;
 		else {
 			ptr = xmalloc(bytes);
-			memcpy(ptr, prev, bucket_sizes[b_idx_old] - sizeof(chunk));
+			memcpy(ptr, prev, bkt->size);
 			xfree(prev);
 		}
 
 	}
-	else {
+	else if (bytes <= PAGE_SIZE) {
+		prev -= sizeof(large_chunk_t);
+
+		ptr = xmalloc(bytes);
+		memcpy(ptr, prev, bkt->size);
+
+		int err = munmap(prev, ((large_chunk_t*)(prev))->size);
+		if (err == -1)
+			perror("xrealloc: munmap() failed");
+
+	}
+	else if (bkt) {
+		bytes += sizeof(large_chunk_t);
+
 		long pages = div_up(bytes, PAGE_SIZE);
 
 		ptr = mmap(NULL, pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 		if (ptr == (void*)(-1))
 			perror("xrealloc: mmap() failed");
 
-		memcpy(ptr, (void*)cPtr, pages * PAGE_SIZE);
+		memcpy(ptr, prev, bkt->size);
 		xfree(prev);
 
-		cPtr = (chunk*)ptr;
-		cPtr->size = bucket_sizes[b_idx_new];
-		cPtr->next = NULL;
+		((large_chunk_t*)(ptr))->size = bytes;
 
-		ptr += sizeof(chunk);
+		ptr += sizeof(large_chunk_t);
+	}
+	else {
+		prev -= sizeof(large_chunk_t);
+		bytes += sizeof(large_chunk_t);
+
+		long pages = div_up(bytes, PAGE_SIZE);
+
+		ptr = mmap(NULL, pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+		if (ptr == (void*)(-1))
+			perror("xrealloc: mmap() failed");
+
+		memcpy(ptr, prev, ((large_chunk_t*)(prev))->size);
+		xfree(prev + sizeof(large_chunk_t));
+
+		((large_chunk_t*)(ptr))->size = bytes;
+		ptr += sizeof(large_chunk_t);
 	}
 
 	return ptr;
@@ -272,20 +347,23 @@ xrealloc(void* prev, size_t bytes)
 void
 dump_buckets()
 {
-	chunk* tmp = NULL;
+	bkt_t* tmp = bkt_list;
 
-	for (int i = 0; i < NUM_BUCKETS; i++) {
-		tmp = buckets[i];
+	if (!tmp)
+		printf("Buckets Empty\n\n");
 
-		printf("%ld Bytes:\n", bucket_sizes[i]);
+	while (tmp) {
+		
+		printf("%lu Bytes:\n", tmp->size);
+		printf("Chunk loc: %p\n", tmp->mem);
 
-		while (tmp) {
-			printf("addr: %p ; size: %lu\n", tmp, tmp->size);
-			tmp = tmp->next;
-		}
+		printf("Used Map: ");
+		for (int i = 7; i >= 0; i--)
+			printf("%lx", tmp->used_map[i]);
 
+		fflush(stdout);
 		printf("\n");
-	}
 
+	}
 }
 
